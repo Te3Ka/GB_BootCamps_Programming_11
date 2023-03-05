f091158c29dceecd61a86d201759a5b388a9db3d
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-03-05 16:46:42 +0300

    Умножение одним потоком

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿const int N = 10;
-const int MIN = 0;
+﻿const int N = 5_000;
+const int MIN = -100;
 const int MAX = 100;
+
+int[,] serialMatrixResult = new int[N, N];
+int[,] threadMatrixResult = new int[N, N];
 
 //Печать массива в консоль
 void PrintArrayToConsole(int[,] printArray)
@@ -12,9 +15,9 @@ void PrintArrayToConsole(int[,] printArray)
     	for(int j = 0; j < printArray.GetLength(1); j++)
     	{
     		if (j == printArray.GetLength(1) - 1)
-        	Console.Write($"{printArray[i, j], 2}");
+        	Console.Write($"{printArray[i, j], 10}");
         else
-        	Console.Write($"{printArray[i, j], 2}, ");
+        	Console.Write($"{printArray[i, j], 10}, ");
     	}
     	Console.WriteLine("]");
     }
@@ -37,18 +40,23 @@ int[,] GenerateMatrix(int rows, int columns)
 
 void serialMatrixPow(int[,] matrFirst, int[,] matrSecond)
 {
-	if (matrFirst.GetLength(1) != matrSecond.GetLength(0) throw new Exception("Нельзя умножать такие матрицы");
-
+	if (matrFirst.GetLength(1) != matrSecond.GetLength(0)) throw new Exception("Нельзя умножать такие матрицы");
+		for(int i = 0; i < matrFirst.GetLength(0); i++)
+		{
+			for(int j = 0; j < matrSecond.GetLength(1); j++)
+			{
+				for(int k = 0; k < matrSecond.GetLength(0); k++)
+				{
+					serialMatrixResult[i, j] += matrFirst[i,k] * matrSecond[k, j];
+				}
+			}
+		}
 }
 
-int[,] serialMatrixResult = new int[N, N];
-int[,] threadMatrixResult = new int[N, N];
-int[,] firstMatrix = GenerateMatrix(N, N);
-int[,] secondMatrix = GenerateMatrix(N, N);
 
-Console.WriteLine("Первая матрица:");
-PrintArrayToConsole(firstMatrix);
-Console.WriteLine();
-Console.WriteLine("Вторая матрица:");
-PrintArrayToConsole(secondMatrix);
-Console.WriteLine();
+int[,] firstMatrix = GenerateMatrix(N, N);
+Console.WriteLine("Первая матрица готова");
+int[,] secondMatrix = GenerateMatrix(N, N);
+Console.WriteLine("Вторая матрица готова");
+serialMatrixPow(firstMatrix, secondMatrix);
+Console.WriteLine("Перемноженная матрица одним потоком готова");
